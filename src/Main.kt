68d3e6e9300c8782bc44dccd7c351bf0bf51f430
Andy Piszyna <a.piszyna@gmail.com>
2020-02-20 19:17:21 +0100

    running

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,7 @@ import java.io.File
 class Main {
 }
 fun main() {
-    val listOfFileNames = listOf("a_example.txt,b_read_on.txt,c_incunabula.txt,d_tough_choices.txt,e_so_many_books.txt,f_libraries_of_the_world.txt")
+    val listOfFileNames = listOf("a_example.txt","b_read_on.txt","c_incunabula.txt","d_tough_choices.txt","e_so_many_books.txt","f_libraries_of_the_world.txt")
     for (fileName in listOfFileNames) {
         processFile(fileName)
     }
