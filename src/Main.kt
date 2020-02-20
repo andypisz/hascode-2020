3f2105994cb6f35ac2a612ef0dba3498aa8c1cc1
Andy Piszyna <a.piszyna@gmail.com>
2020-02-20 17:25:25 +0100

    updated file writing

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,26 @@
 import java.io.File
 
+class Main {
+}
+fun main() {
+    val lines = getLinesOfFile("in/a_example.in")
+    writeFile(lines)
+}
+
 fun getLinesOfFile(fileName: String): List<String> {
     println("getting lines of file: $fileName")
     return File(fileName).readLines()
 }
 
-fun writeFile(fileContent: String) {
-    File("out/output").writeText(fileContent)
+fun writeFile(listOfLines: List<String>) {
+    val content = buildFileContent(listOfLines)
+    File("out/output").writeText(content)
 }
 
-class Main {
-}
-fun main() {
-    getLinesOfFile("in/a_example.in")
-    writeFile("test")
+fun buildFileContent(listOfLines: List<String>): String {
+    var result = ""
+    for (line in listOfLines) {
+        result += "$line\n"
+    }
+    return result
 }
